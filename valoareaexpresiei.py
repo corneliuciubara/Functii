f061c6c72bff50b3833fa8796feb47d5087002ca
corneliuciubara <71380231+corneliuciubara@users.noreply.github.com>
2022-09-14 11:05:22 +0300

    Add files via upload

---
+++ valoareaexpresiei.py
@@ -0,0 +1,10 @@
+n = float(0.5)
+p1 = int(0)
+p2 = int(2)
+p3 = int(4)
+p4 = int(6)
+p5 = int(8)
+def expresie(a, b, c, d, e, f):
+    return a**b+a**c+a**d+a**e+a**f
+print(expresie(n, p1, p2, p3, p4, p5))
+    
