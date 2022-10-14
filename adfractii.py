8c265ea87b944ba7bec83291dbaa062a7441bb71
corneliuciubara <71380231+corneliuciubara@users.noreply.github.com>
2022-10-14 10:53:51 +0300

    Update adfractii.py

--- adfractii.py
+++ adfractii.py
@@ -3,5 +3,5 @@ b = float(input("b="))
 c = float(input("c="))
 d = float(input("d="))
 def inmultirea_fractiilor(x, y, v, z):
-    return x/y*v/z
+    return x/y+v/z
 print(inmultirea_fractiilor(a, b, c, d))
